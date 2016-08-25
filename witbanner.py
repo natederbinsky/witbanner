9b5f06782608b9c825c9c75ae6422b719832865b
Nate Derbinsky <nate.derbinsky@gmail.com>
2016-08-25 15:05:55 -0400

    added [raw] student schedule with latex demo

--- witbanner.py
+++ witbanner.py
@@ -278,6 +278,14 @@ def parse_choosexyz(html):
 	else:
 		return retval
 
+def parse_studentschedule(html):
+	retval = {}
+
+	# for now...
+	retval["raw"] = html
+
+	return retval
+
 ##############################################################################
 ##############################################################################
 
@@ -535,10 +543,17 @@ def banner_idset(xyz):
 	else:
 		return None
 
+def banner_studentschedule():
+	good,r = banner_get("/SSBPROD/bwlkfstu.P_FacStuSchd")
+	if good:
+		return parse_studentschedule(r.text)
+	else:
+		return None
+
 ##############################################################################
 ##############################################################################
 
-def comp1000emails(instructors=None):
+def demo_comp1000emails(instructors=None):
 	codes = banner_sectioncodes("201710")
 	schedules = {name:code for code,name in codes["schedules"].items()}
 
@@ -565,6 +580,73 @@ def comp1000emails(instructors=None):
 	print(emails)
 	print(len(emails))
 
+def _demo_schedlatex_parse(source):
+	ret = []
+
+	soup = BeautifulSoup(source, "lxml")
+	div = soup.findAll('div',attrs={'class':'pagebodydiv'})[0]
+	tables = div.findAll('table', attrs={'class':'datadisplaytable'})
+
+	first = True
+	classId = None
+	for table in tables:
+		if first:
+			caption = table.findAll('caption')[0]
+			classId = caption.string.split(' - ')[1].strip().replace(' ', '')
+			first = False
+		else:
+			row = table.findAll('tr')[1]
+			cols = row.findAll('td')
+			time = [{"hour":int(t[0][0]), "min":int(t[0][1]), "am":t[1]=="am"} for t in [[t[0].split(":"), t[1]] for t in [t.split() for t in cols[1].string.split(' - ')]]]
+			days = cols[2].string
+
+			for day in days:
+				ret.append({"day":str(day), "time":{"from":time[0], "to":time[1]}, "class":str(classId)})
+			first = True
+
+	return ret
+
+def _demo_schedlatex_schedule(events, t="class"):
+	for event in events:
+		day = event["day"].lower() if event["day"]!="R" else "h"
+		title = event["class"]
+		slotbottom = "todo"
+		numslots = "todo"
+
+		toH = event["time"]["to"]["hour"]
+		toM = event["time"]["to"]["min"]
+		toAM = event["time"]["to"]["am"]
+
+		toNum = toH + (0 if toAM else (12 if toH is not 12 else 0)) + (0.0 if toM is 0 else (0.5 if toM <= 30 else 1.0))
+
+		fromH = event["time"]["from"]["hour"]
+		fromM = event["time"]["from"]["min"]
+		fromAM = event["time"]["from"]["am"]
+
+		fromNum = fromH + (0 if fromAM else (12 if fromH is not 12 else 0)) + (0.0 if fromM < 30 else 0.5)
+
+		numslots = int((toNum-fromNum) / 0.5)
+
+		toNumSlot = toNum - 0.5
+		toRN = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"][int(toNumSlot) - 1 - (0 if toNumSlot <= 12 else 12)]
+		toAMPM = "am" if toNumSlot < 12 else "pm"
+		toHalf = "H" if toNumSlot-int(toNumSlot) else ""
+		slotbottom = "\\tc{}{}{}".format(toRN, toAMPM, toHalf)
+
+		print("\\slot{}{{{}}}{{{}}}{{{}}}{{{}}}".format(t, day, slotbottom, "", numslots))
+
+def demo_schedlatex(wids, term):
+	banner_termset(term)
+	for student in wids:
+		xyz = banner_getxyz_wid(term, student)
+		banner_idset(xyz)
+
+		parse = _demo_schedlatex_parse(banner_studentschedule()["raw"])
+		_demo_schedlatex_schedule(parse)
+
+##############################################################################
+##############################################################################
+
 def main(argv):
 	if len(argv) is not 2:
 		print("{} <initial session id>".format(argv[0]))
@@ -584,8 +666,8 @@ def main(argv):
 	# print(banner_sectiontermset("201710"))
 	# print(banner_coursesearch("201710", ["COMP", "MATH"]))
 
-	# comp1000emails()
-	# comp1000emails(["Derbinsky, Nathaniel"])
+	# demo_comp1000emails()
+	# demo_comp1000emails(["Derbinsky, Nathaniel"])
 
 	# print(banner_termset("201710"))
 	# print(banner_adviseelisting())
@@ -597,16 +679,9 @@ def main(argv):
 	# print(banner_getxyz_name("201710", last="xyz"))
 	# print(banner_getxyz_name("201710", last="%"))
 
-	xyz = banner_getxyz_name("201710", last="ricco").keys()[0]
-	print(banner_idset(xyz))
+	demo_schedlatex(("W00325547","W00320677","W00259922"), "201710")
 
-	# params = {
-	# 	"term_in":"",
-	# 	"sname":"bmenu.P_FacStuMnu",
-	# 	"xyz":xyz,
-	# }
-	#
-	# good,r = banner_post("/SSBPROD/bwlkoids.P_FacStoreID", params)
+	# good,r = banner_get("/SSBPROD/bwlkfstu.P_FacStuSchd")
 	# if good:
 	# 	print(r.text)
 	# else:
