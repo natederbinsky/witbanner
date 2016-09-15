e1553d536537680f57dc040d56529885c88c7d8e
Nate Derbinsky <nate.derbinsky@gmail.com>
2016-09-15 18:16:25 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='witbanner',
+      version='0.1',
+      description='WIT Banner Library',
+      author='Nate Derbinsky',
+      author_email='derbinskyn@wit.edu',
+      url='https://derbinsky.info',
+      packages=['witbanner'],
+     )
